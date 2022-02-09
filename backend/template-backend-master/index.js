f6515118c39ee820f8edcf807cb4e94b31b656f9
hochart.dev <hochart.dev@gmail.com>
2022-02-09 13:06:00 +0100

    add post

--- backend/template-backend-master/index.js
+++ backend/template-backend-master/index.js
@@ -4,10 +4,29 @@ const { db } = require('./conf');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-app.get('/Members', async (req, res) => {
+app.get('/members', async (req, res) => {
   const [results] = await db.query('SELECT Name, Quality, Avatar FROM members');
   res.json(results);
+});
+
+app.post('/members', async (req, res) => {
+  const { Name, Quality, Avatar } = req.body;
+  const sql = 'INSERT INTO members (Name, Quality, Avatar) VALUES (?,?,?)';
+  const sqlValues = [Name, Quality, Avatar];
+  try {
+    const [results] = await db.query(sql, sqlValues);
+    res.status(201).json(results);
+  } catch (err) {
+    if (err.code === 'ER_DUP_ENTRY') {
+      return res.status(400).send('This user already exists!');
+    }
+    if (err.code === 'ER_BAD_NULL_ERROR') {
+      return res.status(400).send('Please fill all fields!');
+    }
+    res.status(500).send('Generic error message! ');
+  }
 });
 
 app.use('/', (req, res) => {
